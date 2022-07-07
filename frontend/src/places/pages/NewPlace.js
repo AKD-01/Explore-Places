107844180b371a6d243f0a194b389c7ec70cbe9a
Anjali <83454075+AKD-01@users.noreply.github.com>
2022-07-07 21:41:37 +0530

    Update NewPlace.js

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -6,7 +6,7 @@ import {
   VALIDATOR_MINLENGTH, 
   VALIDATOR_REQUIRE 
 } from '../../shared/util/validators';
-import './NewPlace.css';
+import './PlaceForm.css';
 
 const formReducer = (state, action) => {
   switch (action.type) {
