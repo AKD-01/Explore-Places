c253c0f32380a042efa4f1d12ddbabdd52c572fa
Anjali Kumari Dubey <AKDdubeyanjali9819@gmail.com>
2022-07-08 09:54:11 +0530

    Update NewPlace.js

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useReducer } from 'react';
+import React from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
@@ -6,55 +6,27 @@ import {
   VALIDATOR_MINLENGTH, 
   VALIDATOR_REQUIRE 
 } from '../../shared/util/validators';
+import { useForm } from '../../shared/hooks/form-hook';
 import './PlaceForm.css';
 
-const formReducer = (state, action) => {
-  switch (action.type) {
-    case 'INPUT_CHANGE':
-      let formIsValid = true;
-      for(const inputId in state.inputs){
-        if(inputId === action.inputId){
-          formIsValid = formIsValid && action.isValid;
-        } else {
-          formIsValid = formIsValid && state.inputs[inputId].isValid;
-        }
-      }
-      return {
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.inputId]: {value:action.value, isValid: action.isValid}
-        },
-        isValid: formIsValid
-      };
-    default:
-      return state;
-  }
-};
-
 const NewPlace = () => {
-  const [formState, dispatch] = useReducer(formReducer, {
-    inputs: {
-      title: {
-        value: '',
-        isValid: false
-      },
-      description: {
-        value: '',
-        isValid: false
-      }
+  const [formState, inputHandler] = useForm (
+    {
+    title: {
+      value: '',
+      isValid: false
     },
-    isValid: false 
-  });
-
-  const inputHandler = useCallback((id, value, isValid) => {
-    dispatch({
-      type: 'INPUT_CHANGE', 
-      value: value, 
-      isValid: isValid, 
-      inputId: id
-    });
-  }, []);
+    description: {
+      value: '',
+      isValid: false
+    },
+    address: {
+      value: '',
+      isValid: false
+    }
+  },
+  false
+  );
 
   const placeSubmitHandler = event => {
     event.preventDefault();
