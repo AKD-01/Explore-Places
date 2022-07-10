b6dede748b5312be065f6c21ab2262ccceaeccbe
Anjali Kumari Dubey <AKDdubeyanjali9819@gmail.com>
2022-07-10 10:11:01 +0530

    Update form-hook.js

--- frontend/src/shared/hooks/form-hook.js
+++ frontend/src/shared/hooks/form-hook.js
@@ -19,6 +19,11 @@ const formReducer = (state, action) => {
           },
           isValid: formIsValid
         };
+      case 'SET_DATA':
+          return {
+            inputs: action.inputs,
+            isValid: action.formIsValid
+          };
       default:
         return state;
     }
@@ -38,6 +43,14 @@ export const useForm = (initialInputs, initialFormValidity) => {
           inputId: id
         });
       }, []);
+
+      const setFormData = useCallback((inputData, formValidity) => {
+        dispatch({
+          type: 'SET_DATA',
+          inputs: inputData,
+          formIsValid: formValidity
+        });
+      }, []);
       
-      return [formState, inputHandler];
+      return [formState, inputHandler, setFormData];
 }; 
