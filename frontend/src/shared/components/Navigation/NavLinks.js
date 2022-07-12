52c13348605e779f7b43a94f45040951e82f5722
Anjali Kumari Dubey <AKDdubeyanjali9819@gmail.com>
2022-07-12 10:33:25 +0530

    Update NavLinks.js

--- frontend/src/shared/components/Navigation/NavLinks.js
+++ frontend/src/shared/components/Navigation/NavLinks.js
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 
+import { AuthContext } from '../../context/auth-context';
 import './NavLinks.css';
 
 const NavLinks = props => {
-  return <ul className="nav-links">
+  const auth = useContext(AuthContext);
+  return ( 
+  <ul className="nav-links">
     <li>
-      <NavLink to="/" exact>ALL USERS</NavLink>
+      <NavLink to="/" exact>
+        ALL USERS
+      </NavLink>
     </li>
+    {auth.isLoggedIn && (
     <li>
-      <NavLink to="/u1/places">MY PLACES</NavLink>
+      <NavLink to="/u1/places">
+        MY PLACES
+      </NavLink>
     </li>
+    )}
+    {auth.isLoggedIn && (
     <li>
-      <NavLink to="/places/new">ADD PLACE</NavLink>
+      <NavLink to="/places/new">
+        ADD PLACE
+      </NavLink>
     </li>
+    )}
+    {!auth.isLoggedIn && (
     <li>
-      <NavLink to="/auth">AUTHENTICATE</NavLink>
+      <NavLink to="/auth">
+        AUTHENTICATE
+      </NavLink>
     </li>
+    )}
   </ul>
+  );
 };
 
 export default NavLinks;
